ac44a26a389f48ced4f92d88f44eb3053a1ae358
Svytos <102920568+Svytos@users.noreply.github.com>
2023-03-22 11:40:20 +0200

    container

---
+++ src/Main.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+export default class Main extends Component {
+	render() {
+		return <main className='main'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum corrupti ullam distinctio iure, tenetur molestiae
+			dolore numquam consectetur eum nam laboriosam mollitia, commodi sint quis. Ratione architecto perferendis saepe sit!</main>
+	}
+}
